1c47ed55f6297bef786b0f949b790a0996f916fa
Ondrej Telka <me@ondt.dev>
2022-01-06 00:22:13 +0100

    Implement PNG scaling

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -55,24 +55,20 @@ impl ImageMap {
 	/// # Panics
 	/// TODO
 	#[must_use]
-	pub fn export_png(&self, scale: usize) -> Vec<u8> {
+	pub fn export_png(&self, scale: u16) -> Vec<u8> {
 		let mut opt = usvg::Options::default();
-		// Get file's absolute directory.
-		// opt.resources_dir = std::fs::canonicalize(&args[1]).ok().and_then(|p| p.parent().map(|p| p.to_path_buf()));
 		opt.fontdb.load_system_fonts();
 		
 		let svg = self.export_svg();
 		let rtree = usvg::Tree::from_data(svg.as_bytes(), &opt.to_ref()).unwrap();
 		
-		// let pixmap_size = rtree.svg_node().size.to_screen_size();
-		let pixmap_size = rtree.svg_node().view_box.rect.to_screen_size();
-		let r = rtree.svg_node().view_box.rect;
-		dbg!(r, pixmap_size);
-		// let pixmap_size = ScreenSize::new(r)
+		// why not `rtree.svg_node().size.to_screen_size()`?
+		let size = rtree.svg_node().view_box.rect.to_screen_size();
 		
-		eprintln!("pixmap_size = {:#?}", pixmap_size);
-		let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
-		resvg::render(&rtree, usvg::FitTo::Original, tiny_skia::Transform::default(), pixmap.as_mut()).unwrap();
+		let width = size.width() * u32::from(scale);
+		let height = size.height() * u32::from(scale);
+		let mut pixmap = tiny_skia::Pixmap::new(width, height).unwrap();
+		resvg::render(&rtree, usvg::FitTo::Zoom(f32::from(scale)), tiny_skia::Transform::default(), pixmap.as_mut()).unwrap();
 		
 		pixmap.encode_png().unwrap()
 	}
