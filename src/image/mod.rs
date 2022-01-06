3e416811d54e43169a9708739fda26b39dd2efd6
Ondrej Telka <me@ondt.dev>
2022-01-06 07:14:32 +0100

    Implement map tile rendering

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -1,9 +1,12 @@
 use std::fmt::{Debug, Display, Formatter};
 
+use futures::{stream, StreamExt, TryStreamExt};
+use itertools::Itertools;
 use svg::Document;
+use svg::node::element::Element;
 
 use crate::{BoundingBox, Location, proj};
-use crate::image::loaders::TilesetLoader;
+use crate::image::loaders::{TilesetLoader, TilesetLoaderError};
 
 
 pub mod loaders;
@@ -13,22 +16,40 @@ pub mod loaders;
 pub struct ImageMap {
 	bbox: BoundingBox,
 	zoom: u8,
-	loader: Box<dyn TilesetLoader>,
+	tiles: Vec<Element>,
 	doc: Document,
 	shapes: Vec<Box<dyn Shape + Send + Sync>>,
 }
 
 
 impl ImageMap {
-	#[must_use]
-	pub fn new(bbox: BoundingBox, zoom: u8, loader: impl TilesetLoader + 'static) -> Self {
-		ImageMap {
+	/// # Errors
+	/// TODO
+	pub async fn new(bbox: BoundingBox, zoom: u8, loader: impl TilesetLoader) -> Result<Self, TilesetLoaderError> {
+		let tiles = stream::iter(Self::tile_grid(bbox, zoom))
+			.map(|(x, y)| loader.load_tile(zoom, x, y))
+			.buffer_unordered(8)
+			.try_collect().await?;
+		
+		Ok(ImageMap {
 			bbox,
 			zoom,
-			loader: Box::new(loader),
+			tiles,
 			doc: Document::new(),
 			shapes: Vec::default(),
-		}
+		})
+	}
+	
+	fn tile_grid(bbox: BoundingBox, zoom: u8) -> impl Iterator<Item=(u32, u32)> {
+		let p1 = slippy_map_tilenames::lonlat2tile(bbox.p1.lon, bbox.p1.lat, zoom);
+		let p2 = slippy_map_tilenames::lonlat2tile(bbox.p2.lon, bbox.p2.lat, zoom);
+		
+		let a = p1.0.min(p2.0);
+		let b = p1.0.max(p2.0);
+		let c = p1.1.min(p2.1);
+		let d = p1.1.max(p2.1);
+		
+		(a..=b).cartesian_product(c..=d)
 	}
 	
 	/// Draw a shape on the map.
@@ -50,6 +71,10 @@ impl ImageMap {
 		
 		let mut doc = Document::new()
 			.set("viewBox", (min_x, min_y, width, height));
+		
+		for tile in &self.tiles {
+			doc = doc.add(tile.clone());
+		}
 		
 		for shape in &self.shapes {
 			doc = doc.add(shape.to_element(self.zoom));
